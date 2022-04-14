7e9e44dce3732656f3500665ea359cefbcf808a6
thiago aureliano <thiagoh544@gmail.com>
2022-04-14 19:08:15 -0300

    feat: adiciona array de obj com os pacotes

---
+++ pacotes.js
@@ -0,0 +1,104 @@
+const pacotes = [
+    {
+        name: 'Pacote 1',
+        codigo: '288355555123888',
+        valido: true
+    },
+    {
+        name: 'Pacote 2',
+        codigo: '335333555584333',
+        valido: true
+    },
+    {
+        name: 'Pacote 3',
+        codigo: '223343555124001',
+        valido: true
+    },
+    {
+        name: 'Pacote 4',
+        codigo: '002111555874555',
+        valido: true
+    },
+    {
+        name: 'Pacote 5',
+        codigo: '111188555654777',
+        valido: true
+    },
+    {
+        name: 'Pacote 6',
+        codigo: '111333555123333',
+        valido: true
+    },
+    {
+        name: 'Pacote 7',
+        codigo: '432055555123888',
+        valido: true
+    },
+    {
+        name: 'Pacote 8',
+        codigo: '079333555584333',
+        valido: true
+    },
+    {
+        name: 'Pacote 9',
+        codigo: '155333555124001',
+        valido: true
+    },
+    {
+        name: 'Pacote 10',
+        codigo: '333188555584333',
+        valido: true
+    },
+    {
+        name: 'Pacote 11',
+        codigo: '555288555123001',
+        valido: true
+    },
+    {
+        name: 'Pacote 12',
+        codigo: '111388555123555',
+        valido: true
+    },
+    {
+        name: 'Pacote 13',
+        codigo: '288000555367333',
+        valido: true
+    },
+    {
+        name: 'Pacote 14',
+        codigo: '066311555874001',
+        valido: true
+    },
+    {
+        name: 'Pacote 15',
+        codigo: '110333555123555',
+        valido: true
+    },
+    {
+        name: 'Pacote 16',
+        codigo: '333488555584333',
+        valido: true
+    },
+    {
+        name: 'Pacote 17',
+        codigo: '455448555123001',
+        valido: true
+    },
+    {
+        name: 'Pacote 18',
+        codigo: '022388555123555',
+        valido: true
+    },
+    {
+        name: 'Pacote 19',
+        codigo: '432044555845333',
+        valido: true
+    },
+    {
+        name: 'Pacote 20',
+        codigo: '034311555874001',
+        valido: true
+    }
+]
+
+module.exports = pacotes;
